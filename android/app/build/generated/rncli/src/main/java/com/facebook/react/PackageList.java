28903dce5506f889887c362f44c87063e848d5d2
FawadMahmood <fawad.mahmood@outlook.com>
2022-11-14 18:57:56 +0500

    updates

--- android/app/build/generated/rncli/src/main/java/com/facebook/react/PackageList.java
+++ android/app/build/generated/rncli/src/main/java/com/facebook/react/PackageList.java
@@ -11,6 +11,8 @@ import com.facebook.react.shell.MainReactPackage;
 import java.util.Arrays;
 import java.util.ArrayList;
 
+// @shopify/flash-list
+import com.shopify.reactnative.flash_list.ReactNativeFlashListPackage;
 // react-native-gesture-handler
 import com.swmansion.gesturehandler.RNGestureHandlerPackage;
 // react-native-mmkv
@@ -73,6 +75,7 @@ public class PackageList {
   public ArrayList<ReactPackage> getPackages() {
     return new ArrayList<>(Arrays.<ReactPackage>asList(
       new MainReactPackage(mConfig),
+      new ReactNativeFlashListPackage(),
       new RNGestureHandlerPackage(),
       new MmkvPackage(),
       new ReanimatedPackage(),
